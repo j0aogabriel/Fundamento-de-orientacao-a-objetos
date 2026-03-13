1b24db33fabcbc6b00689bd4ea425689fa54dd0d
João Gabriel Reis dos Santos <reisdossantosjoaogabriel@gmail.com>
2026-03-12 21:57:37 -0300

    Add files via upload

---
+++ Aula 3/aula3_1.cpp
@@ -0,0 +1,9 @@
+#include <iostream> //biblioteca principal do c++
+//iostream sendo importada no meu codigo
+
+int main() {
+    
+   std::cout<<"Hello World em c++";
+
+   return 0; 
+}
---
+++ Aula 3/aula3_2.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    float num1, num2;
+    //Recebendo os dados
+    cout << "Digite o primeiro numero: "; //cout mostra
+    //mensagens ao usuario
+    cin >> num1; //cin libera teclado
+
+    cout << "Digite o segundo numero: ";
+    cin >> num2;
+
+    return 0;
+}
