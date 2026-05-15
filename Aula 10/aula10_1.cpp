e16ec320e9849f8c2f7d6725b29aa815075bdac6
João Gabriel Reis dos Santos <reisdossantosjoaogabriel@gmail.com>
2026-05-15 19:16:55 -0300

    Add files via upload

---
+++ Aula 10/aula10_1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Conta { //classe
+private: //encapsulamento 
+   int numeroConta;
+   string titular;
+   double saldo;
+
+public:
+    // metodo Construtor
+    Conta(int numero, string nome, double saldoInicial) {
+        numeroConta = numero;
+        titular = nome;
+        saldo = saldoInicial;
+    }
+
+    //aula 14/05
+    // Depositar dinheiro metodo
+    void depositar(double valor) {
+        saldo += valor;
+        cout << "Deposito realizado com sucesso!" << endl;
+    }
+
+    // Sacar dinheiro metodo
+    void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+            cout << "Saque realizado com sucesso!" << endl;
+        } else {
+            cout << "Saldo insuficiente!" << endl;
+        }
+    }
+
+    // Exibir dados da conta
+    void exibirDados() {
+        cout << "\n=== DADOS DA CONTA ===" << endl;
+        cout << "Titular: " << titular << endl;
+        cout << "Conta: " << numeroConta << endl;
+        cout << "Saldo: R$" << saldo << endl; 
+    }
+};
